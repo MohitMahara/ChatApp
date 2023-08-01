fae323dbf6ffef127c18ca50b5b36125b76d707d
Mohit <demo144@gmail.com>
2023-08-01 22:34:38 +0530

    initial

--- app.js
+++ app.js
@@ -3,15 +3,18 @@ const app = express();
 const http = require("http").createServer(app);
 const path = require('path');
 const hbs = require("hbs");
+const publicPath = path.join(__dirname, "/public");
+const viewsPath = path.join(__dirname, '/views');
 
 const PORT = process.env.PORT || 8000;
 
 app.set("view engine", "hbs");
-app.use(express.static(path.join(__dirname, "/public") ));
-app.set('views', path.join(__dirname, '/views'));
+app.use(express.static(publicPath));
+app.set('views', viewsPath );
+
 
 app.get('/', (req, res) => {
-    res.render("index");
+    res.render("index.hbs");
 })
 
 
